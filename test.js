1abd2de2a619282779f1304413cac97ab87308b2
Floyd Remmerswaal <floydremmerswaal@gmail.com>
2022-01-31 14:54:55 +0100

    callback works too

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
 import Drive from './index.js';
 
+let cb = (block) => {
+    console.log("I am a callback!");
+    console.log(block);
+} 
+
 let div = document.getElementById("test");
-let myDrive = new Drive({div: div});
+let myDrive = new Drive({div: div, callback: cb});
