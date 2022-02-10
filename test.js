5b568cacca93920118171874aba33bb41c35a517
Floyd Remmerswaal <floydremmerswaal@gmail.com>
2022-02-10 17:13:57 +0100

    remove test

--- test.js
+++
@@ -1,9 +0,0 @@
-import Drive from "./folders.js";
-
-let cb = (block) => {
-  console.log("I am a callback!");
-  console.log(block);
-};
-
-let div = document.getElementById("test");
-let myDrive = new Drive({ div: div, cb: cb });
